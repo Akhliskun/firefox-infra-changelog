5132f248878f9e830492008cd63d62bb8e28de56
bogdan.crisan <bogdan.18rap@gmail.com>
2018-11-06 01:38:08 +0200

    Documented missing pre-filled doc-strings. Refactored a couple of variables and functions.

--- client.py
+++ client.py
@@ -13,8 +13,8 @@ git = Github(TOKEN)
 def create_md_table(project):
     """
     Uses 'project' parameter to generate markdown table.
-    :param project:
-    :return:
+    :param project: Project name
+    :return: MD table in a file.
     """
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -73,7 +73,7 @@ def hg_timestamps_handler(timestamp, timezone):
     return datetime.utcfromtimestamp(ts).strftime("%m-%d-%Y %H:%M:%S")
 
 
-def get_hg_changes(repository_name, push_type):
+def get_hg_changes(hg_repository_name, push_type):
     """
     This function takes a repository and push type and returns a json object that contains changes in that specific
     repository.
@@ -83,12 +83,12 @@ def get_hg_changes(repository_name, push_type):
     example = get_push("https://hg.mozilla.org/build/nagios-tools/", "json-log")
     This will be later used to get the commits from https://hg.mozilla.org/
 
-    :param repository_name: link of the repository, eg: https://hg.mozilla.org/build/nagios-tools/
+    :param hg_repository_name: link of the repository, eg: https://hg.mozilla.org/build/nagios-tools/
     :param push_type: would probably be "json-log" most of the time.
-    :return:
+    :return: returns a json that contains the commits in the provided hg_repository_name
     """
     if push_type == "json-log":
-        request_url = repository_name + push_type
+        request_url = hg_repository_name + push_type
         push_response = requests.get(request_url)
         hg_changes_result = push_response.json()
     else:
@@ -100,7 +100,8 @@ def create_git_link(team, project):
     """
     Expects the name of a project as a parameter and creates the api link for the json.
     We know 'services' is the only project in the mozilla repo while the rest are in mozilla-releng
-    :param project:
+    :param team: github account of the project
+    :param project: name of the project
     :return: GitHub Api link
     """
     beginning_url = "https://api.github.com/repos/"
@@ -109,13 +110,13 @@ def create_git_link(team, project):
     return git_url
 
 
-def get_commits(link):
+def get_git_commits(github_repository_link):
     """
     Makes the json request and return the request result as a list.
-    :param link:
+    :param github_repository_link: link of the github project
     :return: json list
     """
-    commits_response = requests.get(link)
+    commits_response = requests.get(github_repository_link)
     commits_result = commits_response.json()
     return commits_result
 
@@ -123,8 +124,8 @@ def get_commits(link):
 def write_commits(commit_content, file_name):
     """
     Writes commit content to a provided file.
-    :param commit_content:
-    :param file_name:
+    :param commit_content: usually json formatted content
+    :param file_name: name of the file to be written
     :return: none
     """
     with open(file_name, "w") as json_file:
@@ -134,9 +135,8 @@ def write_commits(commit_content, file_name):
 def filter_commit_data(commit):
     """
     Filters out only the data that we need from a commit
-    :param commit:
     Substitute the special characters from commit message using 'sub' function from 're' library
-    :param commit:
+    :param commit: json style content required to be filtered
     :return: filtered json data
     """
     repo_dict = {}
@@ -168,7 +168,7 @@ if __name__ == "__main__":
         repository_name = repositories["Github"][repo]["name"]
         repository_team = repositories["Github"][repo]["team"]
         git_link = create_git_link(repository_team, repository_name)
-        commit_data = get_commits(git_link)
+        commit_data = get_git_commits(git_link)
         useful_data = filter_commit_data(commit_data)
         write_commits(useful_data, "changelog.json")
         create_md_table(repository_name)
