a9e16d427e681c7e8754c2be03c83325a623398c
Roland Mutter <rmutter@mozilla.com>
2019-02-27 01:59:48 +0200

    Client.py cleanup

--- client.py
+++ client.py
@@ -15,14 +15,6 @@ from fic_modules.configuration import (
 from fic_modules.markdown_modules import generate_main_md_table
 
 
-# @click.group()
-# def cli():
-#     """Firefox-Infra-Changelog: tool which build a
-#     changelog of commits happening on git or hg that
-#     could affect Firefox CI Infra"""
-#     pass
-
-
 @click.command()
 @click.option('--all', flag_value='a', default=True,
               help='Run for all currently available repositories')
@@ -35,7 +27,9 @@ from fic_modules.markdown_modules import generate_main_md_table
               help='Let you choose for which repositories the script will run')
 def cli(all, git, hg, l, m):
     from fic_modules.configuration import LOGGER
-
+    """Firefox-Infra-Changelog: tool which build a
+    changelog of commits happening on git or hg that
+    could affect Firefox CI Infra"""
     if l:
         logging.getLogger().addHandler(logging.StreamHandler())
     if all:
